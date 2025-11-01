caae202f23f4c06e7db3293d1d181b9c1116a428
tri-newbie <101498987+tri-newbie@users.noreply.github.com>
2025-11-01 22:16:11 +0800

    Delete src/models/AlphaNet directory

--- src/models/AlphaNet/config.py
+++
@@ -1,2 +0,0 @@
-PARQUET_PATH = '/home/mfin7037_best_students/multi_branch_gru/aveniur-hku_comp/data/hour_data/all_data.parquet'  # Data file path
-DATA_FREQUENCY = '1h'  # Supported values include '1m', '5m', '15m', '30m', '1h', '4h', '1d', etc.
